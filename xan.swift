dd97843ac9f91be94798feffce998a95c4576242
Laurent Boileau <developer@laurentboileau.ca>
2019-03-10 13:49:35 -0400

    XCT Assert snippets
    Signed-off-by: Laurent Boileau <developer@laurentboileau.ca>

---
+++ xan.swift
@@ -0,0 +1,7 @@
+---
+title: "XCT Assert Nil"
+summary: "Assert nil for XCTest"
+completion-scope: Function or Method
+---
+
+XCTAssertNil(<#expression#>, <#message#>)
---
+++ xate.swift
@@ -0,0 +1,9 @@
+---
+title: "XCT Assert Throws Error"
+summary: "Assert throws error for XCTest"
+completion-scope: Function or Method
+---
+
+XCTAssertThrowsError(<#expression#>, <#message#>) { <#error#> in
+    <#statements#>
+}
