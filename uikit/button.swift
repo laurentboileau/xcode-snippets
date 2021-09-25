a63f499da8d223ff93f337eef8dbe981fae37d04
Laurent Boileau <developer@laurentboileau.ca>
2021-09-24 22:32:28 -0400

    Add button snippet

---
+++ uikit/button.swift
@@ -0,0 +1,12 @@
+---
+title: "UIButton stored property"
+summary: "Declares a stored property of type UIButton"
+completion-scope: All
+---
+
+private var <#variable name#>: UIButton = {
+    let view = UIButton()
+    view.translatesAutoresizingMaskIntoConstraints = false
+
+    return view
+}()
